3fc2f5003f5ad319a09368c9a70cb01ff5b7f079
Sidharth Vijayan <sidharth.vijayan2111@gmail.com>
2026-04-15 12:19:46 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,3 @@
 function rewriteText(text) {
-  if (text.length > 300) {
-    return text.split(".").map(t => "• " + t.trim()).join("\n");
-  }
-  return "Answer: " + text;
+  return "Answer: " + text.slice(0, 200);
 }
